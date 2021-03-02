7dddee76cd166799720516dabfd51051fb1bc943
adela <adelabbok@gmail.com>
2021-03-02 18:04:13 +0900

    :bug:Fix: 예외처리 (등록한 작품이 없는 경우) - 등록된 작품이 없습니다 문구가 나오도록 함 - 추후 디자인요소가 추가될 수도 있음
    --- COMMIT END ---

--- src/component/Home/Hot.js
+++ src/component/Home/Hot.js
@@ -1,16 +1,36 @@
 import axios from "axios";
 import styled from "styled-components";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { HomeContext } from "../../../store/homeStore";
 import HotItems from "./HotItems";
+import Nothing from "./Nothing";
 
 const Hot = () => {
   const [state, dispatch] = useContext(HomeContext);
+  const [isExist, setIsExist] = useState(false);
 
   const getList = () => {
-    axios.get("/curation/product").then((res) => {
-      dispatch({ type: "fetchHotPerformances", hot: res.data.hot });
-    });
+    axios
+      .get("/curation/product")
+      .then((res) => {
+        if (Array.isArray(res.data.hot)) {
+          if (res.data.hot.length > 0) {
+            setIsExist(true);
+            dispatch({ type: "fetchHotPerformances", hot: res.data.hot });
+            return;
+          } else {
+            setIsExist(false);
+            return;
+          }
+        } else {
+          setIsExist(false);
+          return;
+        }
+      })
+      .catch((err) => {
+        setIsExist(false);
+        console.error(err);
+      });
   };
 
   useEffect(() => {
@@ -19,7 +39,8 @@ const Hot = () => {
 
   return (
     <Container>
-      <HotItems list={state.hot} />
+      {isExist && <HotItems list={state.hot} />}
+      {!isExist && <Nothing title={"hot"} />}
     </Container>
   );
 };
