35229f06736a51a3a41d161962ab3a1ce9e12455
adela <adelabbok@gmail.com>
2021-03-06 18:50:57 +0900

    :sparkles:Feat:마이페이지 (탭=>페이지) 수정 - 문의 등록 후 마이페이지의 1:1문의로 라우팅하기 위해서 마이페이지를 탭에서 페이지로 수정
    --- COMMIT END ---

--- src/component/Button/GrayShortBtn.js
+++ src/component/Button/GrayShortBtn.js
@@ -1,20 +1,18 @@
 import styled from "styled-components";
 import color from "../../../styles/colors";
 
-const GrayShortBtn = ({ text, onClickHandler }) => {
+const GrayShortBtn = ({ text, onClickHandler, fontColor }) => {
   return <Container onClick={onClickHandler}>{text}</Container>;
 };
 const Container = styled.div`
   width: 100%;
-  /* padding: 0 10px; */
   height: 60px;
   border-radius: 8px;
   background-color: ${color.gray1};
   display: flex;
   align-items: center;
   justify-content: center;
-  color: ${color.black1};
-  opacity: 0.4;
+  color: ${(props) => props.fontColor};
   font-family: "NotoSansCJKkr-Bold";
   font-size: 21px;
   line-height: 18px;
