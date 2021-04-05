315243c0a2d62ae2a5afac89900636f60ac8f1ed
adela <adelabbok@gmail.com>
2021-04-06 05:52:34 +0900

    :sparkles:서비스 소개 페이지 라우팅 연결
    --- COMMIT END ---

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,15 +1,20 @@
+import { useRouter } from "next/router";
 import Section1 from "../../src/component/About/Section1";
 import Section2 from "../../src/component/About/Section2";
 import Section3 from "../../src/component/About/Section3";
 import Section4 from "../../src/component/About/Section4";
 
 export default function About() {
+  const router = useRouter();
+  const changeRouteHandler = (routeStr) => {
+    router.push(routeStr);
+  };
   return (
     <div>
       <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
+      <Section2 changeRouteHandler={changeRouteHandler} />
+      <Section3 changeRouteHandler={changeRouteHandler} />
+      <Section4 changeRouteHandler={changeRouteHandler} />
     </div>
   );
 }
