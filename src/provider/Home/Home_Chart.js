1bc5a8b611be547f73e69f3b63d8dbda25e556da
adela <adelabbok@gmail.com>
2021-04-06 00:09:18 +0900

    :bug: Provider 차트 배포 테스트
    --- COMMIT END ---

--- src/provider/Home/Home_Chart.js
+++ src/provider/Home/Home_Chart.js
@@ -1,4 +1,3 @@
-
 import styled from "styled-components";
 import color from "../../../styles/colors";
 import { useState, useCallback } from "react";
@@ -9,106 +8,91 @@ import {
   XAxis,
   YAxis,
   CartesianGrid,
+  ResponsiveContainer,
   Tooltip,
 } from "recharts";
 
-// import styled from "styled-components";
-// import color from "../../../styles/colors";
-// import { useState, useCallback } from "react";
-// import {
-//   BarChart,
-//   Bar,
-//   Cell,
-//   XAxis,
-//   YAxis,
-//   CartesianGrid,
-//   Tooltip,
-//   Legend,
-//   ResponsiveContainer,
-// } from "recharts";
+const data = [
+  {
+    id: 1,
+    name: "2월",
+    amt: 30000,
+  },
+  {
+    id: 2,
+    name: "3월",
+    amt: 71230,
+  },
+  {
+    id: 3,
+    name: "4월",
+    amt: 123123,
+  },
+];
 
+const Chart = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-// const data = [
-//   {
-//     id: 1,
-//     name: "2월",
-//     amt: 30000,
-//   },
-//   {
-//     id: 2,
-//     name: "3월",
-//     amt: 71230,
-//   },
-//   {
-//     id: 3,
-//     name: "4월",
-//     amt: 123123,
-//   },
-// ];
+  const activeItem = data[activeIndex];
 
-// const Chart = () => {
-//   const [activeIndex, setActiveIndex] = useState(0);
+  const handleClick = useCallback(
+    (entry, index) => {
+      setActiveIndex(index);
+    },
+    [setActiveIndex]
+  );
 
-//   const activeItem = data[activeIndex];
+  const CustomTooltip = ({ active, payload }) => {
+    const tooltipColors = [color.orange, color.blue, color.blue_2];
+    payload.color = tooltipColors;
 
-//   const handleClick = useCallback(
-//     (entry, index) => {
-//       setActiveIndex(index);
-//     },
-//     [setActiveIndex]
-//   );
+    if (active && payload && payload.length) {
+      // console.log(payload[0].payload.id);
+      return (
+        <div className="custom-tooltip">
+          <p
+            className="label"
+            style={{
+              color: payload.color[payload[0].payload.id - 1],
+              backgroundColor: color.white,
+              padding: "14px 6px",
+              borderRadius: "8px",
+              border: `1px solid ${color.black4}`,
+            }}
+          >{`${payload[0].value}원`}</p>
+        </div>
+      );
+    }
 
-//   const CustomTooltip = ({ active, payload }) => {
-//     const tooltipColors = [color.orange, color.blue, color.blue_2];
-//     payload.color = tooltipColors;
+    return null;
+  };
 
-//     if (active && payload && payload.length) {
-//       // console.log(payload[0].payload.id);
-//       return (
-//         <div className="custom-tooltip">
-//           <p
-//             className="label"
-//             style={{
-//               color: payload.color[payload[0].payload.id - 1],
-//               backgroundColor: color.white,
-//               padding: "14px 6px",
-//               borderRadius: "8px",
-//               border: `1px solid ${color.black4}`,
-//             }}
-//           >{`${payload[0].value}원`}</p>
-//         </div>
-//       );
-//     }
+  return (
+    <Container>
+      <ResponsiveContainer width={"100%"} height={"100%"}>
+        <BarChart data={data} margin={0}>
+          {/* <CartesianGrid strokeDasharray="3 3" /> */}
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip content={<CustomTooltip />} />
+          <Bar dataKey="amt" onClick={handleClick} barSize={10}>
+            {data.map((entry, index) => {
+              const cellColors = [color.orange, color.blue, color.blue_2];
+              return (
+                <Cell cursor="pointer" fill={cellColors[index]} key={index} />
+              );
+            })}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+    </Container>
+  );
+};
 
-//     return null;
-//   };
+const Container = styled.div`
+  max-width: 350px;
+  width: 100%;
+  height: 150px;
+`;
 
-//   return (
-//     <Container>
-//       <ResponsiveContainer width={"100%"} height={"100%"}>
-//         <BarChart data={data} margin={0}>
-//           {/* <CartesianGrid strokeDasharray="3 3" /> */}
-//           <XAxis dataKey="name" />
-//           <YAxis />
-//           <Tooltip content={<CustomTooltip />} />
-//           <Bar dataKey="amt" onClick={handleClick} barSize={10}>
-//             {data.map((entry, index) => {
-//               const cellColors = [color.orange, color.blue, color.blue_2];
-//               return (
-//                 <Cell cursor="pointer" fill={cellColors[index]} key={index} />
-//               );
-//             })}
-//           </Bar>
-//         </BarChart>
-//       </ResponsiveContainer>
-//     </Container>
-//   );
-// };
-
-// const Container = styled.div`
-//   max-width: 350px;
-//   width: 100%;
-//   height: 150px;
-// `;
-
-// export default Chart;
+export default Chart;
